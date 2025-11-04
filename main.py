551aa29c5cffae7cd7076892f8df76aa2a1eb737
AnixF <lega.kirill@mail.ru>
2025-11-04 23:44:56 +0300

    Update main.py
    Added top 3 films and rating by genre

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ cursor = connection.cursor()
 
 cursor.execute("CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR, genre VARCHAR) ")
 cursor.execute("CREATE TABLE IF NOT EXISTS ratings (id INTEGER PRIMARY KEY AUTOINCREMENT, movie_id INTEGER, user_id INTEGER, rating INTEGER) ")
+
+fork = int(input("Поиск по жанру - 1 \nПоказать топ 3 - 2: \n"))
+
+if fork == 1:
+    user_genre = input("Введите жанр: ")
+    cursor.execute(f"SELECT movies.genre, AVG(ratings.rating) FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE movies.genre = '{user_genre}'")
+    print(cursor.fetchall())
+elif fork == 2:
+    cursor.execute("SELECT movies.title, AVG(ratings.rating) FROM movies JOIN ratings ON movies.id = ratings.movie_id GROUP BY movies.title ORDER BY ratings.rating DESC LIMIT 3")
+    print(cursor.fetchall())
 connection.commit()
 connection.close()
 input()
